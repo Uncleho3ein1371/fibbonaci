243b1157eafb38a8a7e39872cbf1096b5058128d
hossein <ho3inhatami1371@gmail.com>
2023-09-21 12:41:55 -0400

    is working

--- findFibbo.py
+++ findFibbo.py
@@ -1,20 +1,31 @@
 f_number = 1
 s_number = 2
 def cfibbo():
-
     fibbo_number = f_number + s_number
     return fibbo_number, s_number
+
 
 fibbo_number, s_number = cfibbo()
 third_number = fibbo_number
 
 def fibbocycle():
+    your_range = int(input("please provide your favorite range: "))
+    while your_range<1:
+        your_range = int(input("please provide a number higher than 1: "))
+    print(f_number)
     first_number = s_number
+    print(first_number)
     second_number = third_number
-    while first_number>1:
+    print(second_number)
+    while second_number<your_range:
         fibbo_number1 = first_number + second_number
         print(fibbo_number1)
-        return fibbo_number1, second_number
+        first_number = second_number
+        second_number = fibbo_number1
+
+
+
+
 
 
 
